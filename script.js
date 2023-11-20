b3613c34ba976f3dde3fd2510dc3cd26e3c9c15a
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-11-20 13:33:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const parseCode = (str) => {
-  // your code here
+	let firstZero = str.indexOf("0");
+	let lastZero = str.lastIndexOf("0");
+
+	let regex = /\d([A-Z]|[a-z])+/g;
+	let matched = str.match(regex);
+
+	let obj = {
+	  firstName: str.slice(0, firstZero),
+	  lastName: matched.toString().slice(1),
+	  id: str.slice(lastZero + 1),
+	};
+
+	return obj
 };
 
 // Do not change the code below
